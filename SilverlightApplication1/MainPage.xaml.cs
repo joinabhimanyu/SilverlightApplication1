593c2a90977e3f520daf62703979e2d2c9aa5785
joinabhimanyu <joinabhimanyu@gmail.com>
2015-05-27 17:55:40 +0530

    first commit

---
+++ SilverlightApplication1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightApplication1
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnAnimateClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	EllipseStoryboard.Begin();
+        }
+		
+	    
+		
+    }
+}
